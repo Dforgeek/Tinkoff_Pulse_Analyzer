76b95447dabda85c3b9845fafa13a0c77d8a1ec8
Dforgeek <1812stepanovsky@gmail.com>
2023-01-04 16:50:51 +0300

    feat: finish Parser.py

--- Parser/Parser.py
+++ Parser/Parser.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
 import time
 from bs4 import BeautifulSoup as bs
 import pandas as pd
@@ -6,18 +7,24 @@ import requests
 import os
 from lxml import html
 import re
+import sys
 
-os.chdir('D:/ML/ML_Projects/Tinkoff_Pulse_Emotion_Analyzer/Parser/')
-print(os.getcwd())
-chromedriver_path = 'D:/ML/ML_Projects/Tinkoff_Pulse_Emotion_Analyzer/Parser/chromedriver_win32/chromedriver.exe'
-driver = webdriver.Chrome(executable_path=chromedriver_path)
+chromedriver_path = '/chromedriver_win32/chromedriver.exe'
+chrome_service = ChromeService(executable_path=chromedriver_path)
+chrome_service.start()
+driver = webdriver.Chrome(service=chrome_service)
 
 driver.get(f'https://www.tinkoff.ru/invest/pulse/')
 
 page_length = driver.execute_script("return document.body.scrollHeight")
 
+posts_text = []
+posts_publishers = []
+reactions_number = []
+comments_numbers = []
+post_all = 0
 while page_length < 50000:
-    driver.execute_script(f"window.scrollTo(0, {page_length - 1000});")
+    driver.execute_script(f"window.scrollTo(0, {page_length - 500});")
     page_length = driver.execute_script("return document.body.scrollHeight")
 
     source_data = driver.page_source
@@ -25,49 +32,50 @@ while page_length < 50000:
 
     post_all = soup.find_all("div", {'data-qa-tag': "PulsePost"})
 
-    posts_text = []
-    posts_publishers = []
-    reactions_number = []
-    comments_numbers = []
+for i in post_all:
+    print(i.attrs)
+    print("post_ID: " + i['data-post-id'])
+    author = i.find('a', {'data-qa-type': "uikit/link"}, source=i)
+    print("AuthorLink: " + author['href'] + "//n")
+    post_url = "https://www.tinkoff.ru" + author['href'] + i['data-post-id'] + '/'
 
-    for i in post_all:
-        print(i.attrs)
-        print("post_ID: " + i['data-post-id'])
-        author = i.find('a', {'data-qa-type': "uikit/link"}, source=i)
-        print("AuthorLink: " + author['href'] + "//n")
-        post_url = "https://www.tinkoff.ru" + author['href'] + i['data-post-id'] + '/'
+    response = requests.get(post_url)
+    response.encoding = 'utf-8'
+    html_content = response.text
+    # print("HTML CONTENT: ", html_content)
+    # parse the HTML content
+    soup_inner = bs(html_content, 'html.parser')
+    # print(soup_inner.prettify())
 
-        response = requests.get(post_url)
-        html_content = response.text
+    post = soup_inner.find("div", {'class': ['TextLineCollapse__text_LXa9s', 'PulsePostReviewBody__text_cLzKB']})
+    reactions = soup_inner.find("div", class_="PulsePostReactions__countReactions_fSXGM")
+    comments = soup_inner.find("div", class_="PulsePostReactions__commentText_xvZNw")
 
-        # parse the HTML content
-        soup_inner = bs(html_content, 'html5lib', from_encoding='utf-8')
-        print(soup_inner.prettify())
+    post = post.text
+    # print(post)
+    publisher = author['href'].split('/')[4]
+    react_number = reactions.text
 
-        post = soup_inner.find_all("div", {'class': ['TextLineCollapse__text_LXa9s', 'PulsePostReviewBody__text_cLzKB']})
-        reactions = soup_inner.find_all("div", class_="PulsePostReactions__countReactions_fSXGM")
-        comments = soup_inner.find_all("div", class_="PulsePostReactions__commentText_xvZNw")
+    if comments.text == 'Комментировать':
+        comments_number = 0
+    else:
+        comments_number = comments.text
 
-        post = post.text
-        publisher = author['href'].split('/')[4]
-        react_number = reactions.text
+    posts_text.append(post)
+    posts_publishers.append(publisher)
+    reactions_number.append(react_number)
+    comments_numbers.append(comments_number)
 
-        if comments.text == 'Комментировать':
-            comments_number = 0
-        else:
-            comments_number = comments.text
+    print("SPECIFIC POST WAS READ\n")
 
-        posts_text.append(post)
-        posts_publishers.append(publisher)
-        reactions_number.append(react_number)
-        comments_numbers.append(comments_number)
+print("SCROLLING\nSCROLLING\n")
 
-    df_posts = pd.DataFrame()
-    df_posts['text'] = posts_text
-    df_posts['publisher'] = posts_publishers
-    df_posts['reaction'] = reactions_number
-    df_posts['comment'] = comments_numbers
+df_posts = pd.DataFrame()
+df_posts['text'] = posts_text
+df_posts['publisher'] = posts_publishers
+df_posts['reaction'] = reactions_number
+df_posts['comment'] = comments_numbers
+df_posts = df_posts.drop_duplicates()
+df_posts.to_excel('posts.xlsx', encoding='UTF-8')
 
-    df_posts.to_csv('posts.csv', index=False)
-
-    print('SAVED')
+print('SAVED')
